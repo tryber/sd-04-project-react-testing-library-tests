7891ff861ab5981a299b2246707ccea6129fe3d5
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-06-25 10:54:04 -0300

    initial setup

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,22 +6,25 @@ import './button.css';
 const Button = ({
   className,
   children,
+  dataTestId,
   disabled,
   onClick,
 }) => (
-  <button
-    onClick={onClick}
-    className={`button-text ${className}`}
-    disabled={disabled}
-    type="button"
-  >
-    {children}
-  </button>
-);
+    <button
+      onClick={onClick}
+      className={`button-text ${className}`}
+      disabled={disabled}
+      data-testid={dataTestId}
+      type="button"
+    >
+      {children}
+    </button>
+  );
 
 Button.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  dataTestId: PropTypes.string,
   disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
 };
@@ -29,6 +32,7 @@ Button.propTypes = {
 Button.defaultProps = {
   children: null,
   className: '',
+  dataTestId: undefined,
   disabled: false,
 };
 
